ad70bedb98689921413a7731bd52353e0fcb4837
abenagli <andrea.benaglia@cern.ch>
2017-11-17 11:59:41 +0100

    bug-fixing LinkDef

--- interface/LinkDef.h
+++ interface/LinkDef.h
@@ -1,3 +1,16 @@
-#ifdef __CLING__
+#ifndef CFGMANAGER_LINKDEF_H
+#define CFGMANAGER_LINKDEF_H 1
+
+#include <vector>
+#include <map>
+
+#ifdef __CINT__
+
+#pragma link off all classes;
+#pragma link off all typedef;
+
 #pragma link C++ class CfgManager+;
+
+#endif
+
 #endif
